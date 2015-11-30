e30ffe79fa6b7248fc0f6da82e5262c58ff7c156
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-11-30 12:17:14 -0800

    gone

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('include/config.inc.php');
-session_start();
-session_destroy();
-header( 'Location: /' ) ;
-?>
--- reminderService/cronTest.sh
+++
@@ -1,2 +0,0 @@
-#crontab -e
-18 00 * * * /opt/bitnami/php/bin/php /opt/bitnami/apache2/htdocs-test/mktg/http/reminderService/reminderTest.php
